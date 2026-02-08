230e672246c04bad6edce6b76e260e0d28a988fa
Spakka <vergeriolorenzo@gmail.com>
2026-02-08 18:22:17 +0100

    ..

---
+++ criptoproject/decripters.py
@@ -0,0 +1,38 @@
+
+ciao = ["a", "b", "c", "d", "e", "f", "g", "h"]
+
+
+def get_decr_indx(numbers:list):
+
+    cont = 0
+
+    if numbers[0]=='£' and numbers[1]=='£' and numbers[2]=='%' and numbers[3]=='/' and numbers[4]=='$':
+        return cont + 1
+    if numbers[0]=='&' and numbers[1]=='%' and numbers[2]=='1' and numbers[3]=='1' and numbers[4]==';':
+        return cont + 2
+    if numbers[0]=='|' and numbers[1]=='|' and numbers[2]=='0' and numbers[3]=='&' and numbers[4]=='_':
+        return cont + 3
+    
+
+
+def deleteprefix(numbers:list):
+
+    for i in range(5):
+        numbers.pop(0)
+
+    return
+
+
+def decripter1(numbers:list):
+    pass
+
+def decripter2(numbers:list):
+    pass
+
+def decripter3(numbers:list):
+    pass
+
+
+        
+
+    
--- criptoproject/main.py
+++ criptoproject/main.py
@@ -1,2 +1,5 @@
 from cripters import criptalettere1, criptalettere2, criptalettere3
+from decripters import decripter1, decripter2, decripter3, get_decr_indx, deleteprefix
+
+
 

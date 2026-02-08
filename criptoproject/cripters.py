a5385a59cea3eb939c54183cf9307b38d7d45024
Spakka <vergeriolorenzo@gmail.com>
2026-02-08 13:27:33 +0100

    aggiunto cripter 2 e modfica finale

--- criptoproject/cripters.py
+++ criptoproject/cripters.py
@@ -13,135 +13,273 @@ def criptalettere1(passw: str):
                 numbers.append("|")
             else:               #bar vert e punto escl
                 numbers.append("!")
-        if passw[i] == 'b' :
+        elif passw[i] == 'b' :
             if random.randint(1, 5) %2 == 0:
                 numbers.append("£")
             else:               #pund e perc
                 numbers.append("%")
-        if passw[i] == 'c' :
+        elif passw[i] == 'c' :
             if random.randint(1, 5) %2 == 0:
                 numbers.append("^")
             else:               #pot e punto dom
                 numbers.append("?")
-        if passw[i] == 'd' :
+        elif passw[i] == 'd' :
             if random.randint(1, 5) %2 == 0:
                 numbers.append("+")
             else:               #piu e per
                 numbers.append("*")
-        if passw[i] == 'e' :
+        elif passw[i] == 'e' :
             if random.randint(1, 5) %2 == 0:
                 numbers.append("{")
             else:               #graffa ap quadra chius
                 numbers.append("]")
-        if passw[i] == 'f':
+        elif passw[i] == 'f':
             if random.randint(1, 5) %2 == 0:
                 numbers.append("}")
             else:               #graf chius quadra apt
                 numbers.append("[")
-        if passw[i] == 'g':
+        elif passw[i] == 'g':
             if random.randint(1, 5) %2 == 0:
                 numbers.append("(")
             else:               #quadara apt e dollaro
                 numbers.append("$")
-        if passw[i] == 'h':
+        elif passw[i] == 'h':
             if random.randint(1, 5) %2 == 0:
                 numbers.append("/")
             else:               #norm slash . e comm
                 numbers.append("&")
-        if passw[i] == 'i':
+        elif passw[i] == 'i':
             if random.randint(1, 5) %2 == 0:
                 numbers.append("ì")
             else:               #i acc e e acc
                 numbers.append("è")
-        if passw[i] == 'j':
+        elif passw[i] == 'j':
             if random.randint(1, 5) %2 == 0:
                 numbers.append(")")
             else:               #quadra chius e ugu
                 numbers.append("=")
-        if passw[i] == 'k':
+        elif passw[i] == 'k':
             if random.randint(1, 5) %2 == 0:
                 numbers.append("#")
             else:               #canc e chiocc
                 numbers.append("@")
-        if passw[i] == 'l':
+        elif passw[i] == 'l':
             if random.randint(1, 5) %2 == 0:
                 numbers.append("°")
             else:               #gradi virgola
                 numbers.append(",")
-        if passw[i] == 'm': #mezooooooooooooo
+        elif passw[i] == 'm': #mezooooooooooooo
             if random.randint(1, 5) %2 == 0:
                 numbers.append(":")
             else:               #du punt e punt
                 numbers.append(".")
-        if passw[i] == 'n':
+        elif passw[i] == 'n':
             if random.randint(1, 5) %2 == 0:
                 numbers.append(";")
             else:               #punt virgola e tratt 
                 numbers.append("-")
-        if passw[i] == 'o':
+        elif passw[i] == 'o':
             if random.randint(1, 5) %2 == 0:
                 numbers.append("_")
             else:               #tratt low u acc
                 numbers.append("ù")
-        if passw[i] == 'p':
+        elif passw[i] == 'p':
             if random.randint(1, 5) %2 == 0:
                 numbers.append("§")
             else:               #s strana e c rumena
                 numbers.append("ç")
-        if passw[i] == 'q' :
+        elif passw[i] == 'q' :
             if random.randint(1, 5) %2 == 0:
                 numbers.append("1")
             else:               #1 e 8
                 numbers.append("8")
-        if passw[i] == 'r':
+        elif passw[i] == 'r':
             if random.randint(1, 5) %2 == 0:
                 numbers.append("<")
             else:               #magg e minore
                 numbers.append(">")
-        if passw[i] == 's' :
+        elif passw[i] == 's' :
             if random.randint(1, 5) %2 == 0:
                 numbers.append("7")
             else:               # 7 e  3
                 numbers.append("3")
-        if passw[i] == 't' :
+        elif passw[i] == 't' :
             if random.randint(1, 5) %2 == 0:
                 numbers.append("6")
             else:               #5 e 6
                 numbers.append("5")
-        if passw[i] == 'u' :
+        elif passw[i] == 'u' :
             if random.randint(1, 5) %2 == 0:
                 numbers.append("Ñ")
             else:               #egne maiusc e minusc
                 numbers.append("ñ")
-        if passw[i] == 'v' :
+        elif passw[i] == 'v' :
             if random.randint(1, 5) %2 == 0:
                 numbers.append("ɛl")
             else:               #l norvegese
                 numbers.append("ɛl")
-        if passw[i] == 'w' :
+        elif passw[i] == 'w' :
             if random.randint(1, 5) %2 == 0:
                 numbers.append("kʉ")
             else:               #q norvegese
                 numbers.append("kʉ")
-        if passw[i] == 'x' :
+        elif passw[i] == 'x' :
             if random.randint(1, 5) %2 == 0:
                 numbers.append("æ")
             else:               #strana norvegese
                 numbers.append("æ")
-        if passw[i] == 'y' :
+        elif passw[i] == 'y' :
             if random.randint(1, 5) %2 == 0:
                 numbers.append("ø")
             else:               #o norvegese
                 numbers.append("ø")
-        if passw[i] == 'z' :
+        elif passw[i] == 'z' :
             if random.randint(1, 5) %2 == 0:
                 numbers.append("å")
             else:               #a norvegese
                 numbers.append("å")
-
+        else:
+            numbers.append(f"cc{passw[i]}")
     return
 
 
 def criptalettere2(passw: str):
-    pass
+    numbers = []
+    lenght = len(passw)
+
+    for i in range(lenght):
+
+        if passw[i] == 'a':
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("!")
+            else:               #bar vert e punto escl
+                numbers.append("|")
+        elif passw[i] == 'b' :
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("%")
+            else:               #pund e perc
+                numbers.append("£")
+        elif passw[i] == 'c' :
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("?")
+            else:               #pot e punto dom
+                numbers.append("^")
+        elif passw[i] == 'd' :
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("*")
+            else:               #piu e per
+                numbers.append("*")
+        elif passw[i] == 'e' :
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("]")
+            else:               #graffa ap quadra chius
+                numbers.append("{")
+        elif passw[i] == 'f':
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("[")
+            else:               #graf chius quadra apt
+                numbers.append("}")
+        elif passw[i] == 'g':
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("$")
+            else:               #quadara apt e dollaro
+                numbers.append("(")
+        elif passw[i] == 'h':
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("&")
+            else:               #norm slash . e comm
+                numbers.append("/")
+        elif passw[i] == 'i':
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("è")
+            else:               #i acc e e acc
+                numbers.append("ì")
+        elif passw[i] == 'j':
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("=")
+            else:               #quadra chius e ugu
+                numbers.append(")")
+        elif passw[i] == 'k':
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("@")
+            else:               #canc e chiocc
+                numbers.append("#")
+        elif passw[i] == 'l':
+            if random.randint(1, 5) %2 == 0:
+                numbers.append(",")
+            else:               #gradi virgola
+                numbers.append("°")
+        elif passw[i] == 'm': #mezooooooooooooo
+            if random.randint(1, 5) %2 == 0:
+                numbers.append(".")
+            else:               #du punt e punt
+                numbers.append(":")
+        elif passw[i] == 'n':
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("-")
+            else:               #punt virgola e tratt 
+                numbers.append(";")
+        elif passw[i] == 'o':
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("ù")
+            else:               #tratt low u acc
+                numbers.append("_")
+        elif passw[i] == 'p':
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("ç")
+            else:               #s strana e c rumena
+                numbers.append("§")
+        elif passw[i] == 'q' :
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("8")
+            else:               #1 e 8
+                numbers.append("1")
+        elif passw[i] == 'r':
+            if random.randint(1, 5) %2 == 0:
+                numbers.append(">")
+            else:               #magg e minore
+                numbers.append("<")
+        elif passw[i] == 's' :
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("3")
+            else:               # 7 e  3
+                numbers.append("7")
+        elif passw[i] == 't' :
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("5")
+            else:               #5 e 6
+                numbers.append("6")
+        elif passw[i] == 'u' :
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("ñ")
+            else:               #egne maiusc e minusc
+                numbers.append("Ñ")
+        elif passw[i] == 'v' :
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("ɛl")
+            else:               #l norvegese
+                numbers.append("ɛl")
+        elif passw[i] == 'w' :
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("kʉ")
+            else:               #q norvegese
+                numbers.append("kʉ")
+        elif passw[i] == 'x' :
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("æ")
+            else:               #strana norvegese
+                numbers.append("æ")
+        elif passw[i] == 'y' :
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("ø")
+            else:               #o norvegese
+                numbers.append("ø")
+        elif passw[i] == 'z' :
+            if random.randint(1, 5) %2 == 0:
+                numbers.append("å")
+            else:               #a norvegese
+                numbers.append("å")
+        else:
+            numbers.append(f"cc{passw[i]}")
+    return
 

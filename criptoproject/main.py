480ed6b054d2a4c88c047178f713de55bed1a06b
Spakka15 <lorenzovergerio@libero.it>
2026-02-09 22:00:28 +0100

    aggiunte inizio

--- criptoproject/main.py
+++ criptoproject/main.py
@@ -3,3 +3,22 @@ from decripters import decripter1, decripter2, decripter3, get_decr_indx, delete
 
 
 
+def creatutto():
+    print("""
+    Benvenuto nel cripter fenomenale di Spakka_14!
+    Per iniziare inserisci le tue credenziali per accedere al database!
+     
+""")
+    
+def afetrlogin(user: str):
+    print(f"""
+    Benvenuto {user}!
+    Scegli azione da effettuare:
+    1) Visuallizza credenzili accesso
+    2) Crea nuove credenziali d'accesso
+    3) Modifica credenziali d'accesso
+    4) Elimina credenziale d'accesso
+    5) esci
+    """)
+
+
